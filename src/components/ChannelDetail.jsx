3a186d39c4dc58a1dc16e0c2b91298a530e76daa
yourcoldeyes <normatovmirakbar30@gmail.com>
2023-07-12 16:53:47 +0500

    yes!

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+
+	const [ChannelDetail, setChannelDetail] = useState(null);
+	const [videos, setVideos] = useState([]);
+
+	const { id } = useParams();
+
+	useEffect(() => {
+		fetchFromAPI(`channels?.part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+		fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+	}, [id])
+	
+
+	return (
+		<Box minHeight='95vh'>
+			<Box>
+			<div style={{   background: 'linear-gradient(to right, #8a2387, #e94057, #f27121)', zIndex: 10, height: '300px', 
+		}} />
+			<ChannelCard channelDetail={ ChannelDetail } marginTop='-110px' />
+			</Box>
+			<Box display="flex" p='2' >
+				<Box sx={{ mr: { sm: '100px' } }} />
+					<Videos videos={videos} />
+			</Box>
+		</Box>
+	)
+}
+
+export default ChannelDetail
